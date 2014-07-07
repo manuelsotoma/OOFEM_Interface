06a75f57ca122e4a84cd5da0defbd0d7c2503a07
manuelsotoma <manuelsotoma@hotmail.com>
2014-07-07 03:19:47 +0200

    https://github.com/manuelsotoma/OOFEM_Interface.git

---
+++ BeamComponent/BeamComponentInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace safeprojectname
+{
+    public class infoclassname : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "BeamComponent";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("$4561aeb8-65fa-4cc3-b3b0-97974848d19e$");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "";
+            }
+        }
+    }
+}
